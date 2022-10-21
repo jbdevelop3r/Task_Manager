c1ce9b991f04b66d4aa1f15dd4723270aa5c6c9e
jbdevelop3r <justinebanogon14@gmail.com>
2022-10-21 22:53:25 +0800

    login_designs

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,6 +5,20 @@ class TasksController < ApplicationController
         redirect_to category_path(@category)
     end
 
+    def edit
+        @task = Task.find(params[:id])
+      end
+    
+      def update
+        @task = Task.find(params[:id])
+    
+        if @task.update(task_params)
+          redirect_to @category, alert: "task has been updated"
+        else
+          render :edit, status: :unprocessable_entity
+        end
+      end
+
     def destroy
         @category = Category.find(params[:category_id])
         @task = @category.tasks.find(params[:id])
