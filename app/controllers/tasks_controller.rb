074616eb9a991ea22f6b4adc925497b2a79126ca
jbdevelop3r <justinebanogon14@gmail.com>
2022-10-21 21:03:48 +0800

    delete_task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,14 @@ class TasksController < ApplicationController
         @comment = @category.tasks.create(task_params)
         redirect_to category_path(@category)
     end
-    
+
+    def destroy
+        @category = Category.find(params[:category_id])
+        @task = @category.tasks.find(params[:id])
+        @task.destroy
+        redirect_to category_path(@category), status: :see_other
+      end
+     
     private
 
     def task_params
