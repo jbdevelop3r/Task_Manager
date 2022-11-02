7663fbf7b1333a1a8196a5955616eccb38f38785
jbdevelop3r <justinebanogon14@gmail.com>
2022-11-02 17:29:28 +0800

    testing

--- test/system/categories_test.rb
+++ test/system/categories_test.rb
@@ -17,22 +17,43 @@ class CategoriesTest < ApplicationSystemTestCase
   end
 
   test "visit index" do
+    visit categories_url
     assert_selector "p", text: "Categories"
   end
 
   test "should create a category" do
-  
+    visit categories_url
     click_on "Add a category"
-  
     fill_in "category[title]", with: "Creating a category"
-  
     click_on "Create Category"
-
     assert_text "A category has been created."
-  
     assert_text "Creating a category"
   end
 
+  test "showing a category" do
+    visit categories_url
+    Capybara.page.find('.category-name').click
+    assert_text "You have no task.."
+  end
+
+  test "Update a category" do
+    visit categories_url
+    Capybara.page.find('.category-name').click
+    Capybara.page.find('.category-links-edit').click match: :first
+    fill_in "category[title]", with: "Updated"
+    click_on "Update Category"
+    assert_text "A category has been updated."
+    assert_text "Updated"
+  end
+
+  test "Delete a category" do
+    Capybara.page.find('.category-name').click
+    page.accept_confirm do
+      Capybara.page.find('.category-links-delete').click
+    end
+    assert_text "A category has been deleted."
+  end
 end
+
 
 
