e1f0da4afcd0229f1414f413e2fb93f8e9043b9f
jbdevelop3r <justinebanogon14@gmail.com>
2022-11-02 14:26:52 +0800

    testing

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -4,11 +4,6 @@ require "capybara/cuprite"
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400] 
   # driven_by :cuprite
-  def login_as(user, password)
-    visit new_user_session_path
-    fill_in 'user_email', with: user.email
-    fill_in  'user_password', with: password
-    click_on 'Log in'
-  end
+ 
 end
 
--- test/system/categories_test.rb
+++ test/system/categories_test.rb
@@ -2,22 +2,37 @@ require "application_system_test_case"
 
 class CategoriesTest < ApplicationSystemTestCase
   include Devise::Test::IntegrationHelpers
-  def setup
-    sign_in users(:one)
-    @category = categories(:one)
+
+  def login_as(user, password)
+    visit new_user_session_path
+    fill_in 'user_email', with: user.email
+    fill_in  'user_password', with: password
+    click_on 'Log in'
+  end
+  
+  setup do
+    @category = categories(:first_category)
+    @user = users(:justine)
+    login_as(@user, 'justine') 
+  end
+
+  test "visit index" do
+    assert_selector "p", text: "Categories"
   end
 
   test "should create a category" do
-    visit categories_url
   
     click_on "Add a category"
   
     fill_in "category[title]", with: "Creating a category"
   
     click_on "Create Category"
+
+    assert_text "A category has been created."
   
     assert_text "Creating a category"
   end
+
 end
 
 
